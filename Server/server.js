9909e06365f19b71bd8362b068418edc5078fc83
Sayra Flores <sayraflores238@gmail.com>
2022-07-24 21:10:31 -0400

    1st

--- Server/server.js
+++ Server/server.js
@@ -2,25 +2,27 @@ const express = require('express'); // creating an express server which requires
 const app = express();
 const mysql = require('mysql'); // requiring mysql dependency
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Justin_0309',
+    password: '123456789',
     database: 'NstalgiaProducts'
 })
 
-// app.use(cors());
-// app.use(express.json());
-// app.listen(3001,()=> { // check to see if localhost:3000 is running
-//     console.log('running on port 3001');
-// })
-// db.connect(err => {
-//     if(err) {
-//         return err
-//     }
-//     else {console.log('Connected!! :)')}
-// })
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.listen(3001,()=> { // check to see if localhost:3001 is running
+    console.log('running on port 3001');
+})
+db.connect(err => {
+    if(err) {
+        console.log(err)
+    }
+    else {console.log('Connected!! :)')}
+})
 
 // req = request, res = response that will be sent to frontend 
 app.get('/Products', (req, res)=> {
---
+++ src/pages/ProductTemp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductTemp = () => {
+  return (
+    <div>ProductTemp</div>
+  )
+}
+
+export default ProductTemp
